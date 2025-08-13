f93955a1b77fa3715686fd6b9a62df0c98cc7f12
Troitskii Georgii <gtroitsk@redhat.com>
2025-08-13 11:35:41 +0200

    Add tests for Hibernate reactive validation modes

---
+++ sql-db/hibernate-reactive/src/test/java/ValidationNoneIT.java
@@ -0,0 +1,32 @@
+import io.quarkus.test.bootstrap.PostgresqlService;
+import io.quarkus.test.bootstrap.RestService;
+import io.quarkus.test.scenarios.QuarkusScenario;
+import io.quarkus.test.services.Container;
+import io.quarkus.test.services.QuarkusApplication;
+
+@QuarkusScenario
+public class ValidationNoneIT {
+
+    private static final String POSTGRES_USER = "quarkus_test";
+    private static final String POSTGRES_PASSWORD = "quarkus_test";
+    private static final String POSTGRES_DATABASE = "quarkus_test";
+    private static final int POSTGRES_PORT = 5432;
+
+    @Container(image = "${postgresql.latest.image}", port = POSTGRES_PORT, expectedLog = "listening on IPv4 address")
+    static PostgresqlService database = new PostgresqlService()
+            .withUser(POSTGRES_USER)
+            .withPassword(POSTGRES_PASSWORD)
+            .withDatabase(POSTGRES_DATABASE)
+            .withProperty("PGDATA", "/tmp/psql");
+
+    @QuarkusApplication
+    static RestService app = new RestService().withProperties("postgresql.properties")
+            .withProperty("quarkus.datasource.username", POSTGRES_USER)
+            .withProperty("quarkus.datasource.password", POSTGRES_PASSWORD)
+            .withProperty("quarkus.datasource.reactive.url", database::getReactiveUrl)
+            .withProperty("quarkus.hibernate-orm.validation.mode", "none");;
+
+    protected RestService getApp() {
+        return app;
+    }
+}
